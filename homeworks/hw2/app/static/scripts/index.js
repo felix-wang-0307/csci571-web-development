b2cb5e0208983a180c415a0b2e440bd414458120
felix-wang-0307 <hwang779@usc.edu>
2024-10-09 01:08:04 -0700

    Refactor .gitignore file to exclude unnecessary files and directories

--- homeworks/hw2/app/static/scripts/index.js
+++ homeworks/hw2/app/static/scripts/index.js
@@ -1,19 +1,23 @@
 import { clearForm, submitForm, toggleAutoDetect } from "./form.js";
 
-document.getElementById("weather-form").addEventListener('submit', submitForm);
-document.getElementById("weather-form").addEventListener('reset', clearForm);
-document.getElementById("auto-detect").addEventListener('click', toggleAutoDetect);
+document.getElementById("weather-form").addEventListener("submit", submitForm);
+document.getElementById("weather-form").addEventListener("reset", clearForm);
+document
+  .getElementById("auto-detect")
+  .addEventListener("click", toggleAutoDetect);
 
 const pointDown = document.querySelector(".point-down");
 const pointUp = document.querySelector(".point-up");
 
-pointDown.addEventListener('click', () => {
+pointDown.addEventListener("click", () => {
   pointDown.style.display = "none";
   pointUp.style.display = "flex";
   document.querySelector("#weather-charts .charts").style.display = "flex";
+  // scroll the page to let the top being the "weather-charts" section
+  window.scrollTo(0, document.querySelector("#weather-charts").offsetTop);
 });
 
-pointUp.addEventListener('click', () => {
+pointUp.addEventListener("click", () => {
   pointUp.style.display = "none";
   pointDown.style.display = "flex";
   document.querySelector("#weather-charts .charts").style.display = "none";
